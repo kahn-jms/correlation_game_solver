a43b85e805a8c13c50cc7059167c4863c68fbc35
jkahn <kahn.jms@gmail.com>
2016-04-02 03:28:22 +0200

    Can now start a game, can't seem to enter a username though.
    Pretty sure it's to do with the latest Chrome updates, will test on
    Firefox when I get a chance. Basically it's the alert.send_text('...')
    that's not working, all other alert functions seem to work.

--- correlation-guess.py
+++ correlation-guess.py
@@ -26,20 +26,30 @@ chromedriver_location = '/usr/lib/chromium-browser/chromedriver'
 driver = webdriver.Chrome(chromedriver_location)
 
 # Go to the page we want to visit
-#driver.get("http://guessthecorrelation.com/")
-driver.get("http://www.python.org/")
+driver.get("http://guessthecorrelation.com/")
 
 #assert "Python" in driver.title
-time.sleep(5)
 # elem = driver.find_element_by_name("q")
 # elem.send_keys("pycon")
 # elem.send_keys(Keys.RETURN)
 # assert "No results found." not in driver.page_source
 
+################################################################################
+### Start a game
+################################################################################
 ## Greeted with the start menu, need to start a new game
 elem_new_game = driver.find_element_by_id("new-game")
 elem_new_game.click()
+time.sleep(5)
 
+## Need to create a username initially
+alert=driver.switch_to_alert()
+alert.accept()
+
+
+################################################################################
+### Play the game
+################################################################################
 ## Only need to fetch the game control elements once since they're persistent
 # Guess box controls the guess entry (duh)
 elem_guess = driver.find_element_by_id("guess-input")
@@ -52,11 +62,13 @@ elem_guess.send_keys(corr_guess)
 # And submit the guess
 elem_guess.send_keys(Keys.RETURN)
 # This takes us to the result page
+time.sleep(5)
 
 # Next we need to click on the NEXT button
 # The guess element is hidden so sendin keys will not work because
 # we're not dealing with an <input> element
 elem_next.click()
+time.sleep(5)
 
 
 driver.close()
